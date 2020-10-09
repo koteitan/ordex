251c8886e4336855422204d799e79b6bebcc456e
koteitan <ukaibutton@mac.com>
2020-10-10 08:50:40 +0900

    implement parse/change name of test.js into main.js

--- kuma3ary.js
+++ kuma3ary.js
@@ -1,2 +1,7 @@
 // kuma kuma 3-ary Psi
+Kuma3ary = function(){
+};
 
+Kuma3ary.parse = function(str){
+  return Ordinal.parse_md_paren_bracket(new Kuma3ary(), str);
+}
--- ordinal.js
+++ ordinal.js
@@ -1,27 +1,40 @@
-/* ordinal object. */
+/* ordinal abstruct object. */
 Ordinal = function(){};
-/* parse text and output into the fields of o.
+
+/* parse text as Multi-Dimensional parenthesis and bracket 
+ * and output into the fields of o.
  * o.m = memory
  * o.b[i] = bracket list */
-Ordinal.prototype.parse = function(o, text){
+Ordinal.parse_md_paren_bracket = function(o, text){
   //parse memory
-  var m=[[]];
   //              1  2   3   4
   var r = /\(?[\s\d,()]*\)?/;
   var matched=text.match(r);
-  matched=matched.replaceAll("\(","[");
-  matched=matched.replaceAll("\)","]");
-  var m=JSON.parse(matched);
-  while(m.length==1) m=m[0]; //defrate no-width dimension
-  o.m = m;
+  if(matched!=null){
+    matched=matched[0].replaceAll("\(","[");
+    matched=matched.replaceAll("\)","]");
+    o.m=JSON.parse(matched);
+    while(o.m.length==1) o.m=o.m[0]; //defrate no-width dimension
+  }
 
   //parse brackets
-  var ci=0;
   var r = /((\[[\s\d]*\])|\s)+/;
   var matched=text.match(r);
   if(matched!=null){
-    o.b = JSON.parse(matched.replace(/\]\s*\[/g,","));
+    o.b = JSON.parse(matched[0].replace(/\]\s*\[/g,","));
   }else{
     o.b = null; 
   }
+  return o;
+}
+Ordinal.toString_md_paren_bracket = function(o){
+  var str="";
+  for(var x=0;x<o.length;x++){
+    str+="(";
+    for(var y=0;y<o[x].length;y++){
+      str+=o[x][y];
+      if(y<o[x].length-1) str+=",";
+    }
+  }
+  return str;
 }
