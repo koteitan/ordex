62fcc28be98c8e1c62792fb4705d494788a7c7b9
koteitan <ukaibutton@mac.com>
2021-01-01 16:32:30 +0900

    multiple rows/output sugared result and tree

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 window.onload=function(){ //entry point
   
-  var o=new Kuma3ary("w");
-  intext.value = o.toString();
+  intext.value = "0\n1\n2\nw";
 };
 var dothemall=function(){ //button
   //input
-  var str=intext.value;
-  //trim
-  str=str.replace(/[\n\s]/g, "");
-  //parse
-  var o=Kuma3ary.parse(str);
-  //out
-  outtext.value=o.toString();
+  var mstr=intext.value;
+  //split
+  var astr=mstr.split("\n");
+  outtext.value+="";
+  for(var i=0;i<astr.length;i++){
+    //trim
+    var str=astr[i].replace(/[\s]/g, "");
+    //parse
+    var o=Kuma3ary.parse(str);
+    //out
+    outtext.value+=o.toString(Kuma3ary.toSugar);
+    outtext.value+=" = ";
+    outtext.value+=o.toTree();
+    outtext.value+="\n";
+  }
 };
 k0=new Kuma3ary("0");
 k1=new Kuma3ary("1");
