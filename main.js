0aad359692c031357a0c94489d4310d28bf991e6
koteitan <ukaibutton@mac.com>
2021-01-01 21:37:14 +0900

    v0.5./parse supports multiple columns./compare supports single columns.

--- main.js
+++ main.js
@@ -13,23 +13,60 @@ var parse=function(){
   //input
   var mstr=intext.value;
   //split
-  var astr=mstr.split("\n");
+  var ystr=mstr.split("\n");
   
   outtext.value="";
-  for(var i=0;i<astr.length;i++){
+  for(var y=0;y<ystr.length;y++){
     //trim
-    var str=astr[i].replace(/[\s]/g, "");
+    var str=ystr[y].replace(/\s\s*/g, " ");
+    str=str.replace(/^\s*/g, "");
+    str=str.replace(/\s*$/g, "");
     if(str!=""){
-      //parse
-      var o=Kuma3ary.parse(str);
-      //out string expression
-      outtext.value+=o.toString(Kuma3ary.toSugar);
-      //out tree expression
-      outtext.value+=" = ";
-      outtext.value+=o.toTree();
+      var xstr=str.split(" ");
+      for(var x=0;x<xstr.length;x++){
+        str=xstr[x];
+        if(str!=""){
+          //parse
+          var o=Kuma3ary.parse(str);
+          //out string expression
+          outtext.value+=o.toString(Kuma3ary.toSugar);
+          //out tree expression
+        }
+        outtext.value+="  ";
+      }//x
     }
     outtext.value+="\n";
-  }
+  }//y
+};
+
+var drawtree=function(){
+  //input
+  var mstr=intext.value;
+  //split
+  var ystr=mstr.split("\n");
+  
+  outtext.value="";
+  for(var y=0;y<ystr.length;y++){
+    //trim
+    var str=ystr[y].replace(/\s\s*/g, " ");
+    str=str.replace(/^\s*/g, "");
+    str=str.replace(/\s*$/g, "");
+    if(str!=""){
+      var xstr=str.split(" ");
+      for(var x=0;x<xstr.length;x++){
+        str=xstr[x];
+        if(str!=""){
+          //parse
+          var o=Kuma3ary.parse(str);
+          //out string expression
+          outtext.value+=o.toTree();
+          //out tree expression
+        }
+        outtext.value+="  ";
+      }//x
+    }
+    outtext.value+="\n";
+  }//y
 };
 /** compare()
   * @brief parse intext and output result into outtext.
@@ -44,22 +81,27 @@ var compare=function(){
   outtext.value="";
   for(var i=0;i<astr.length;i++){
     //trim
-    var str2=astr[i].replace(/^[\s]/g, "");
-    var str2=astr[i].replace(/[\s]$/g, "");
-    var str2=astr[i].replace(/\s\s*/g, " ");
+    var str2=astr[i].replace(/^\s*/g , "" );
+    var str2=   str2.replace(/\s*$/g , "" );
+    var str2=   str2.replace(/\s\s*/g, " ");
     if(str2!=""){
       var astr2=str2.split(" ");
       //parse
-      var x=Kuma3ary.parse(astr2[0]);
-      var y=Kuma3ary.parse(astr2[1]);
-      //out string expression
-      var c;
-      if     (Kuma3ary.lessthan(x,y)) c = " < ";
-      else if(Kuma3ary.equal   (x,y)) c = " = ";
-      else                            c = " > ";
-      outtext.value+=x.toString(Kuma3ary.toSugar);
-      outtext.value+=c;
-      outtext.value+=y.toString(Kuma3ary.toSugar);
+      if(astr2.length>=2){
+        var x=Kuma3ary.parse(astr2[0]);
+        var y=Kuma3ary.parse(astr2[1]);
+        //out string expression
+        var c;
+        if     (Kuma3ary.lessthan(x,y)) c = " < ";
+        else if(Kuma3ary.equal   (x,y)) c = " = ";
+        else                            c = " > ";
+        outtext.value+=x.toString(Kuma3ary.toSugar);
+        outtext.value+=c;
+        outtext.value+=y.toString(Kuma3ary.toSugar);
+      }else{
+        var x=Kuma3ary.parse(astr2[0]);
+        outtext.value+=x.toString(Kuma3ary.toSugar);
+      }
     }
     outtext.value+="\n";
   }
